e366ead72e12635779787625f16a164d44b9e613
metjinze-ro <220328y@ugs.kochi-tech.ac.jp>
2021-01-18 16:52:48 +0900

    アカウント作成画面の実装

--- app/src/main/java/com/example/reguseraccount/MainActivity2.java
+++ app/src/main/java/com/example/reguseraccount/MainActivity2.java
@@ -1,14 +1,25 @@
 package com.example.reguseraccount;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Button;
+import android.widget.EditText;
 import android.os.Bundle;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    private EditText name_MultiLine;
+    private EditText nickname_Multiline;
+    private EditText ID_Multiline;
+    private EditText new_password;
+    private EditText new_password_re;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        name_MultiLine = findViewById(R.id.name_MultiLine);
+        nickname_Multiline = findViewById(R.id.nickname_Multiline);
+        ID_Multiline = findViewById(R.id.ID_Multiline);
+        new_password = findViewById(R.id.new_password);
+        new_password_re = findViewById(R.id.new_password_re);
     }
 }
