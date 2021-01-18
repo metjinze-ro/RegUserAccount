3a26801cce6036a51fcf1ee6d05d2a81743a8ec6
metjinze-ro <220328y@ugs.kochi-tech.ac.jp>
2021-01-18 16:33:15 +0900

    エミュレータで表示できるようにした

--- app/src/main/java/com/example/reguseraccount/MainActivity.java
+++ app/src/main/java/com/example/reguseraccount/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.reguseraccount;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextTextEmailAddress;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextTextEmailAddress = findViewById(R.id.editTextTextEmailAddress);
+
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        Button button = findViewById(R.id.button_login);
+        Button button1 = findViewById(R.id.button_accountCreate);
     }
 }
